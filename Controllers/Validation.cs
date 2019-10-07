7d71b081455bfd400cf1281fba406b2fa96e16bc
LizHitches <liz.hitches@gmail.com>
2019-10-07 17:21:05 -0400

    lab21

---
+++ Controllers/Validation.cs
@@ -0,0 +1,26 @@
+﻿using FirstMVCapp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMVCapp.Controllers
+{
+ //   public IActionResult ControllerSide(string gender)
+ //   {
+ //       if (gender == "Female")
+ //       {
+ //           return ViewBag.Genderm("You Go Girl ");
+ //       }
+
+ //       if (gender == "Male")
+ //       {
+ //           return ViewBag.Genderm("Boy O Boy  ");
+ //       }
+ //       else
+ //       {
+ //           return ViewBag.Genderm("You Got This");
+ //       }
+ //   }
+ }
---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMVCapp.Models
+{
+    public class User
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+
+
+        public string Password { get; set; }
+
+    }
+}
